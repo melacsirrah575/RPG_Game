903b30259028c31380f2776a98ea9da623985bfd
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-26 09:25:34 -0400

    Created combat target and using raycast to check if enemy is selected

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using RPG.Movement;
+using RPG.Combat;
 
 //Starting namespace with RPG. in case I bring in anything with the same namespace later on
 namespace RPG.Control
@@ -12,6 +13,30 @@ namespace RPG.Control
 
         private void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            //Instead of having an 'out' return, RaycastAll returns a list of things hit
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            //Looping through all objects Raycast hit to look for enemy
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                //Continue carrys on in the loop but skips the body it was put it
+                if (target == null) continue;
+                //Don't want player to be able to hold down mouse to attack
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
@@ -20,16 +45,21 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            //Takes position from where player clicked on MainCamera's near clipping plane and sets as variable
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Inlined variable
             RaycastHit hit;
             //out hit stores Raycast hit position in hit
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            //Takes position from where player clicked on MainCamera's near clipping plane and sets as variable
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
