2e1c0988d42da4a53e1ba4c7c946bbe6d4501d58
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-29 09:26:30 -0400

    Adding a trigger to play intro animation

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool hasBeenTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player" && !hasBeenTriggered)
+            {
+                hasBeenTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+
+            }
+        }
+    }
+}
