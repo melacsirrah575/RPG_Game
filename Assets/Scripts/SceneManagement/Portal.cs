28f2d889bfb7c34a58019f84d842a3d2ad009dc2
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-29 15:30:41 -0400

    Player now spawns at the proper corresponding portal

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -9,9 +9,15 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
         [Tooltip("Cannot be less than 0")]
         [SerializeField] int sceneToLoad = -1; //Initialized to -1 to throw error if forgotten to change in editor
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -23,6 +29,12 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("You forgot to assign a scene to load.");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
 
             //Calls Coroutine again once scene has finished loading
@@ -38,6 +50,7 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
+                if (portal.destination != destination) continue;
                 return portal;
             }
 
@@ -47,6 +60,7 @@ namespace RPG.SceneManagement
         private void UpdatePlayer(Portal otherPortal)
         {
             GameObject player = GameObject.FindWithTag("Player");
+            //Using NavMeshAgent to prevent conflict between it and manual placement
             player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
             player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
