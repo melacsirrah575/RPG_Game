19688605bd2b46e49ecff90f263412b27545e128
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-29 15:18:18 -0400

    Adding Spawnpoints for player when they pass through a portal

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
@@ -9,6 +11,7 @@ namespace RPG.SceneManagement
     {
         [Tooltip("Cannot be less than 0")]
         [SerializeField] int sceneToLoad = -1; //Initialized to -1 to throw error if forgotten to change in editor
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,10 +24,31 @@ namespace RPG.SceneManagement
         private IEnumerator Transition()
         {
             DontDestroyOnLoad(gameObject);
+
             //Calls Coroutine again once scene has finished loading
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
     }
 }
