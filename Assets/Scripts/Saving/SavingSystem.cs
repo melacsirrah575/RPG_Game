e6080cc87ec4a91ec065656769b5404b89e24f63
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-30 15:30:52 -0400

    Creating SaveableEntity script to assign objects as saveable

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -19,11 +19,9 @@ namespace RPG.Saving
             //Adding using prevents possible errors causing file to not close and instead, leak
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GetPlayerTransform();
                 //Have to create method before able to call methods from it
                 BinaryFormatter formatter = new BinaryFormatter();
-                SerializableVector3 position = new SerializableVector3(playerTransform.position);
-                formatter.Serialize(stream, position);
+                formatter.Serialize(stream, CaptureState());
             }
         }
 
@@ -34,38 +32,28 @@ namespace RPG.Saving
             //FileMode.Open doesn't overwrite any existing file
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-
-                Transform playerTransform = GetPlayerTransform();
                 BinaryFormatter formatter = new BinaryFormatter();
-                SerializableVector3 position = (SerializableVector3)formatter.Deserialize(stream);
-                playerTransform.position = position.ToVector();
+                RestoreState(formatter.Deserialize(stream));
             }
         }
 
-        Transform GetPlayerTransform()
+        private object CaptureState()
         {
-            return GameObject.FindWithTag("Player").transform;
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
+            }
+            return state;
         }
 
-        byte[] SerializeVector(Vector3 vector)
+        private void RestoreState(object state)
         {
-            //3 bytes at a length of 4
-            byte[] vectorBytes = new byte[3 * 4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            return vectorBytes;
-        }
-
-        Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 result = new Vector3();
-            //ToSingle is basically the same as ToFloat
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-            return result;
-
+            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+            }
         }
 
         string GetPathFromSaveFile(string saveFile)
