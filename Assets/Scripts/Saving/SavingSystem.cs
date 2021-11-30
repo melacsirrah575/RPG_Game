31e1108b578944f698541b98dcca150be809dba4
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-30 16:25:29 -0400

    Update SavingSystem.cs
    Positions are saved between scenes

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -13,6 +13,33 @@ namespace RPG.Saving
 
         public void Save(string saveFile)
         {
+            Dictionary<string, object> state = LoadFile(saveFile);
+            CaptureState(state);
+            SaveFile(saveFile, state);
+        }
+
+        public void Load(string saveFile)
+        {
+            RestoreState(LoadFile(saveFile));
+        }
+
+        private Dictionary<string, object> LoadFile(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            if(File.Exists(path))
+            {
+                return new Dictionary<string, object>();
+            }
+            //FileMode.Open doesn't overwrite any existing file
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (Dictionary<string, object>)formatter.Deserialize(stream);
+            }
+        }
+
+        private void SaveFile(string saveFile, object state)
+        {
             string path = GetPathFromSaveFile(saveFile);
             print("Saving to" + path);
             //Creates new file and overwrites old file if one exists
@@ -21,38 +48,28 @@ namespace RPG.Saving
             {
                 //Have to create method before able to call methods from it
                 BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(stream, CaptureState());
+                formatter.Serialize(stream, state);
             }
         }
 
-        public void Load(string saveFile)
+        private void CaptureState(Dictionary<string, object> state)
         {
-            string path = GetPathFromSaveFile(saveFile);
-            print("Loading from" + path);
-            //FileMode.Open doesn't overwrite any existing file
-            using (FileStream stream = File.Open(path, FileMode.Open))
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                RestoreState(formatter.Deserialize(stream));
-            }
-        }
-
-        private object CaptureState()
-        {
-            Dictionary<string, object> state = new Dictionary<string, object>();
             foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
             {
                 state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
             }
-            return state;
         }
 
-        private void RestoreState(object state)
+        private void RestoreState(Dictionary<string, object> state)
         {
-            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
             foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
             {
-                saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+               string id = saveable.GetUniqueIdentifier();
+               if(state.ContainsKey(id))
+               {
+                    saveable.RestoreState(state[id]);
+               }
+
             }
         }
 
