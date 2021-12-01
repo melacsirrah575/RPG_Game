3c4d9c9cbe4cbfdf788b234945208850d6a789be
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-30 23:56:45 -0400

    Save State should be up and working properly

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RPG.Saving
+using RPG.Saving;
+
+namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
+        [SerializeField] [Range(0f, 2f)] float fadeInTime = 0.5f;
+
         const string defaultSaveFile = "save";
 
         private IEnumerator Start()
         {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
             yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
         }
 
         private void Update()
