f482822b3eadfc6c84e8680fb18ca9c4132393ac
melacsirrah575 <melacsirrah575@gmail.com>
2021-11-30 23:05:00 -0400

    Duplicating SaveableEntities in scene no longer duplicates their UUIDs. Player's UUID now consistent between scenes

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 using RPG.Core;
+using System;
 
 namespace RPG.Saving
 {
@@ -12,6 +13,8 @@ namespace RPG.Saving
     public class SaveableEntity : MonoBehaviour
     {
         [SerializeField] string uniqueIdentifier = "";
+        static Dictionary<string, SaveableEntity> globalLookup = new Dictionary<string, SaveableEntity>();
+
         public string GetUniqueIdentifier()
         {
             return uniqueIdentifier;
@@ -50,11 +53,34 @@ namespace RPG.Saving
             SerializedObject serializedObject = new SerializedObject(this);
             SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
             
-            if (string.IsNullOrEmpty(property.stringValue))
+            if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))
             {
                 property.stringValue = System.Guid.NewGuid().ToString();
                 serializedObject.ApplyModifiedProperties();
             }
+
+            globalLookup[property.stringValue] = this;
+        }
+
+        private bool IsUnique(string candidate)
+        {
+            if (!globalLookup.ContainsKey(candidate)) return true;
+
+            if (globalLookup[candidate] == this) return true;
+
+            if (globalLookup[candidate] == null)
+            {
+                globalLookup.Remove(candidate);
+                return true;
+            }
+
+            if (globalLookup[candidate].GetUniqueIdentifier() != candidate)
+            {
+                globalLookup.Remove(candidate);
+                return true;
+            }
+
+            return false;
         }
 #endif
     }
