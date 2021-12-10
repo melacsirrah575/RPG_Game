7774cfd4a8ad68c10a114d0b4018ea74ae5bf354
melacsirrah575 <melacsirrah575@gmail.com>
2021-12-10 09:34:25 -0400

    Tweaking Damage text and beginning to add healthbars for enemies

--- Assets/Scripts/UI/DamageText/DamageText.cs
+++ Assets/Scripts/UI/DamageText/DamageText.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 namespace RPG.UI.DamageText
 {
     public class DamageText : MonoBehaviour
     {
-        [SerializeField] Text damageText = null;
+        [SerializeField] TextMeshProUGUI damageText = null;
         public void SetValue(float value)
         {
             damageText.text = String.Format("{0,0}", value);
