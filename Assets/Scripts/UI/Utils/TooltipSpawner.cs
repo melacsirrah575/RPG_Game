45c94d74aeb5fc563008a78c53d4c8ff74321054
melacsirrah575 <melacsirrah575@gmail.com>
2021-12-15 22:02:46 -0400

    Adding a Tooltip spawner

---
+++ Assets/Scripts/UI/Utils/TooltipSpawner.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Inventory.Tooltips
+{
+    //Abstract based class that handles the spawning of a tooltip prefab at the correct position on screen relative to a cursor
+    
+    public abstract class TooltipSpawner : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [Tooltip("The prefab of the tooltip to spawn.")]
+        [SerializeField] GameObject tooltipPrefab = null;
+
+        GameObject tooltip = null;
+
+        //Called when it is time to update the infoon the tooltip
+        public abstract void UpdateTooltip(GameObject tooltip);
+
+        //Returns true when the tooltip spawner should be allowed to create a tooltip
+        public abstract bool CanCreateTooltip();
+
+        private void OnDestroy()
+        {
+            ClearTooltip();
+        }
+
+        private void OnDisable()
+        {
+            ClearTooltip();
+        }
+
+        void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+        {
+            var parentCanvas = GetComponentInParent<Canvas>();
+
+            if (tooltip && !CanCreateTooltip())
+            {
+                ClearTooltip();
+            }
+
+            if (!tooltip && CanCreateTooltip())
+            {
+                tooltip = Instantiate(tooltipPrefab, parentCanvas.transform);
+            }
+
+            if (tooltip)
+            {
+                UpdateTooltip(tooltip);
+                PositionTooltip();
+            }
+        }
+
+        private void PositionTooltip()
+        {
+            //Required to ensure corners are updated by positioning elements
+            Canvas.ForceUpdateCanvases();
+
+            var tooltipCorners = new Vector3[4];
+            tooltip.GetComponent<RectTransform>().GetWorldCorners(tooltipCorners);
+            var slotCorners = new Vector3[4];
+            GetComponent<RectTransform>().GetWorldCorners(slotCorners);
+
+            bool below = transform.position.y > Screen.height / 2;
+            bool right = transform.position.x < Screen.width / 2;
+
+            int slotCorner = GetCornerIndex(below, right);
+            int tooltipCorner = GetCornerIndex(!below, !right);
+
+            tooltip.transform.position = slotCorners[slotCorner] - tooltipCorners[tooltipCorner] + tooltip.transform.position;
+        }
+
+        private int GetCornerIndex(bool below, bool right)
+        {
+            if (below && !right) return 0;
+            else if (!below && !right) return 1;
+            else if (!below && right) return 2;
+            else return 3;
+        }
+
+        void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+        {
+            ClearTooltip();
+        }
+
+        private void ClearTooltip()
+        {
+            if (tooltip)
+            {
+                Destroy(tooltip.gameObject);
+            }
+        }
+    }
+}
