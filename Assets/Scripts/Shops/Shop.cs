bd78b129411f5aeb924cd071d6e470ee41c03aa1
melacsirrah575 <melacsirrah575@gmail.com>
2022-01-07 16:04:41 -0400

    Items bought in shop are now put in inventory. Excess items return to the store and do not charge the player

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -23,8 +23,14 @@ namespace RPG.Shops
         }
 
         Dictionary<InventoryItem, int> transaction = new Dictionary<InventoryItem, int>();
+        Shopper currentShopper = null;
 
         public event Action onChange;
+
+        public void SetShopper(Shopper shopper)
+        {
+            this.currentShopper = shopper;
+        }
 
         public IEnumerable<ShopItem> GetFilteredItems() 
         {
@@ -41,7 +47,25 @@ namespace RPG.Shops
         public void SelectMode(bool isBuying) { }
         public bool IsBuyingMode() { return true; }
         public bool CanTransact() { return true; }
-        public void ConfirmTransaction() { }
+        public void ConfirmTransaction() 
+        {
+            Inventory shopperInventory = currentShopper.GetComponent<Inventory>();
+            if (shopperInventory == null) return;
+
+            var transactionSnapshot = new Dictionary<InventoryItem, int>(transaction);
+            foreach (InventoryItem item in transactionSnapshot.Keys)
+            {
+                int quantity = transactionSnapshot[item];
+                for (int i = 0; i < quantity; i++)
+                {
+                    bool success = shopperInventory.AddToFirstEmptySlot(item, 1);
+                    if (success)
+                    {
+                        AddToTransaction(item, -1);
+                    }
+                }
+            }
+        }
 
         public string GetShopName()
         {
