67f3c1f48268e27cc863005ba3f7458970a056e7
melacsirrah575 <melacsirrah575@gmail.com>
2022-01-11 16:03:17 -0400

    Created a TriggerAnimation effect and an Orient To Target effect

--- Assets/Scripts/Attributes/ManaDisplay.cs
+++ Assets/Scripts/Attributes/ManaDisplay.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 namespace RPG.Attributes
 {
     public class ManaDisplay : MonoBehaviour
     {
         Mana mana;
+        TextMeshProUGUI text;
 
         private void Awake()
         {
             mana = GameObject.FindGameObjectWithTag("Player").GetComponent<Mana>();
+            text = GetComponent<TextMeshProUGUI>();
         }
 
         private void Update()
         {
-            GetComponent<Text>().text = string.Format("{0:0}/{1:0}", mana.GetMana(), mana.GetMaxMana());
+            text.text = String.Format("{0:0}/{1:0}", mana.GetMana(), mana.GetMaxMana());
         }
     }
 }
