add9dc0557b334b8137eeb2844e4b8a20841d133
melacsirrah575 <melacsirrah575@gmail.com>
2022-01-13 16:36:23 -0400

    Resetting Animation and enemies on death

--- Assets/Scripts/Control/Respawner.cs
+++ Assets/Scripts/Control/Respawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.AI;
+using Cinemachine;
 
 using RPG.Attributes;
 using RPG.SceneManagement;
@@ -14,11 +15,20 @@ namespace RPG.Control
         [SerializeField] Transform respawnLocation;
         [SerializeField] float respawnDelay = 3;
         [SerializeField] float fadeTime = 0.2f;
-        [SerializeField] float healthRegenPercentage = 20;
+        [SerializeField] float healthRegenPercentage = 20f;
+        [SerializeField] float enemyHealthRegenPercentage = 20f;
 
         private void Awake()
         {
             GetComponent<Health>().onDie.AddListener(Respawn);
+        }
+
+        private void Start()
+        {
+            if (GetComponent<Health>().IsDead())
+            {
+                Respawn();
+            }
         }
 
         private void Respawn()
@@ -28,13 +38,42 @@ namespace RPG.Control
 
         private IEnumerator RespawnRoutine()
         {
+            SavingWrapper savingWrapper = FindObjectOfType<SavingWrapper>();
+            savingWrapper.Save();
             yield return new WaitForSeconds(respawnDelay);
             Fader fader = FindObjectOfType<Fader>();
             yield return fader.FadeOut(fadeTime);
+            RespawnPlayer();
+            ResetEnemies();
+            savingWrapper.Save();
+            yield return fader.FadeIn(fadeTime);
+        }
+
+        private void ResetEnemies()
+        {
+            foreach (AIController enemyControllers in FindObjectsOfType<AIController>())
+            {
+                enemyControllers.Reset();
+                Health health = enemyControllers.GetComponent<Health>();
+                if (health && !health.IsDead())
+                {
+                    health.Heal(health.GetMaxHealthPoints() * enemyHealthRegenPercentage / 100);
+                }
+            }
+        }
+
+        private void RespawnPlayer()
+        {
+            Vector3 positionDelta = respawnLocation.position - transform.position;
+
             GetComponent<NavMeshAgent>().Warp(respawnLocation.position);
             Health health = GetComponent<Health>();
             health.Heal(health.GetMaxHealthPoints() * healthRegenPercentage / 100);
-            yield return fader.FadeIn(fadeTime);
+            ICinemachineCamera activeVirtualCamera = FindObjectOfType<CinemachineBrain>().ActiveVirtualCamera;
+            if (activeVirtualCamera.Follow == transform)
+            {
+                activeVirtualCamera.OnTargetObjectWarped(transform, positionDelta);
+            }
         }
     }
 }
