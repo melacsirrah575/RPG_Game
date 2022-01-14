5335be317a9b10a44297dca636c9bc485a7d7768
melacsirrah575 <melacsirrah575@gmail.com>
2022-01-14 14:26:51 -0400

    Creating money pickups

--- Assets/Scripts/Inventory/Wallet.cs
+++ Assets/Scripts/Inventory/Wallet.cs
@@ -7,7 +7,7 @@ using RPG.Saving;
 
 namespace RPG.Inventories
 {
-    public class Wallet : MonoBehaviour, ISaveable
+    public class Wallet : MonoBehaviour, ISaveable, IItemStore
     {
         [SerializeField] float startingBalance = 400f;
 
@@ -42,6 +42,16 @@ namespace RPG.Inventories
         public void RestoreState(object state)
         {
             balance = (float)state;
+        }
+
+        public int AddItems(InventoryItem item, int number)
+        {
+            if (item is CurrencyItem)
+            {
+                UpdateBalance(item.GetPrice() * number);
+                return number;
+            }
+            return 0;
         }
     }
 }
